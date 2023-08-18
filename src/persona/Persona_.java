ee936e02af7fba8bcac8ee923b86755508acf1bc
JhonPalominoC <142623871+JhonPalominoC@users.noreply.github.com>
2023-08-18 14:10:02 -0500

    Add files via upload

---
+++ src/persona/Persona_.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persona;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona_ {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the edad
+     */
+    public String getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+     public String nombre;
+     public String apellido;
+     public String dni;
+     public String edad;
+    
+    
+}
